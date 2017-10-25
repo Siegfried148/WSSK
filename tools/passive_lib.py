a79137962212fd0956fbc7513cd34356b6d83a74
Siegfried148 <virgilio.castro.rendon@gmail.com>
2017-10-25 11:19:14 -0500

    Minor changes

--- tools/passive_lib.py
+++ tools/passive_lib.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 #Castro Rendón Virgilio
 from requests import get, options
+from ssl import create_default_context, CERT_NONE
+from socket import socket
 
 
 def check_methods(url):
     try:
-        response = options(url, verify=False)
+        response = options(url, verify=False, timeout=6)
         if 'allow' in response.headers:
             http_methods = response.headers['allow']
         else:
             http_methods = 'Could not determine HTTP methods'
         return {'http_methods':http_methods}
     except Exception as e:
-        return {'error1': 'Passive Analysis Error: %s' % 'Are the port and protocol correct?'}
+        return {'error1': 'Passive Analysis Error:(%s) ' % url +'Is the information correct?'}
 
 
 
@@ -21,21 +23,21 @@ def check_index(url):
         indexes = ['index.html','index.htm','index.php','index.asp', 'index.phtml', 'index.cgi', 'index.xhtml']
         index_result = ''
         for index in indexes:
-            response = get('%s/%s' % (url,index), verify=False)
+            response = get('%s/%s' % (url,index), verify=False, timeout=6)
             if response.status_code == 200:
                 index_result += '%s, ' % index
         return {'index_files':index_result[:-2]}
     except Exception as e:
-        return {'error1': 'Passive Analysis Error: %s' % 'Are the port and protocol correct?'}
+        return {'error1': 'Passive Analysis Error:(%s) ' % url +'Is the information correct?'}
 
 
 def check_robots(url):
     try:
-        response = get('%s/%s' % (url,'robots.txt'), verify=False)
+        response = get('%s/%s' % (url,'robots.txt'), verify=False, timeout=6)
         robots_file = 'Has a robots.txt file' if response.status_code == 200 else 'Does not have a robots.txt'
         return {'robots_file':robots_file}
     except Exception as e:
-        return {'error1': 'Passive Analysis Error: %s' %  'Are the port and protocol correct?'}
+        return {'error1':  'Passive Analysis Error:(%s) ' % url +'Is the information correct?'}
 
 
 def check_install(url):
@@ -43,19 +45,19 @@ def check_install(url):
         directories = ['setup','install']
         dirs_result = ''
         for d in directories:
-            response = get('%s/%s' % (url,d), verify=False)
+            response = get('%s/%s' % (url,d), verify=False, timeout=6)
             if response.status_code in [200,403]:
                 dirs_result += '%s, ' % d
         result_dir = {'install_dir':dirs_result[:-2]} if dirs_result != '' else {'install_dir':'No installation directories'}
         return result_dir
     except Exception as e:
-        return {'error1': 'Passive Analysis Error: %s' %  'Are the port and protocol correct?'}
+        return {'error1': 'Passive Analysis Error:(%s) ' % url +'Is the information correct?'}
 
 
 
 def check_headers(url):
     try:
-	    response = get(url, verify=False)
+	    response = get(url, verify=False, timeout=6)
 	    headers = response.headers
 	    signature = headers['Server'] if 'Server' in headers else 'Header is not set'
 	    php_version = headers['X-Powered-By'] if 'X-Powered-By' in headers else 'Header is not set'
@@ -82,11 +84,45 @@ def check_headers(url):
 	            'setcookie_secure':setcookie_secure,
                     'setcookie_httponly':setcookie_httponly}
     except Exception as e:
-        return {'error2': 'HTTP Header Analysis Error: %s' %  'Are the port and protocol correct?'}
+        return {'error2': 'HTTP Analysis Error:(%s) ' % url +'Is the information correct?'}
 
 
 
-#def check_certificate()
+def get_domain(cert):
+    domain = ''
+    if 'subjectAltName' in cert:
+        for value in cert['subjectAltName']:
+            if value[0] == 'DNS':
+                domain += '%s, ' % value[1]
+        return {'cert_domain': domain[:-2]}
+
+def get_ca(cert):
+    ca = ''
+    if 'issuer' in cert:
+#        for value in cert['issuer']:
+#            if value[0] == 'commonName':
+#                ca += '%s, ' % value[1]
+        return {'cert_ca': cert['issuer']}
+
+def check_certificate(ip, port=443):
+    try:
+        ctx = create_default_context()
+        ctx.check_hostname = False
+        s = ctx.wrap_socket(socket(), server_hostname=ip)
+        s.connect((ip, port))
+        cert = s.getpeercert()
+     
+        result = {}
+        result.update(get_domain(cert))
+        result.update(get_ca(cert))
+        result.update({'ssl_suites':cert})
+
+        return result
+        
+    except Exception as e:
+        return {'cert_domain':e}
+
+
 
 def passive_analysis(ip, port, protocol):
     try:
@@ -104,7 +140,13 @@ def passive_analysis(ip, port, protocol):
         result_dict.update(check_index(url))
         result_dict.update(check_robots(url))
         result_dict.update(check_install(url))
+        if protocol == 'HTTPS':
+            result_dict.update(check_certificate(ip, int(port)))
+        else:
+            result_dict.update(check_certificate(ip))
+
 
         return result_dict
     except ValueError as e:
-        return {'passive_ip':ip,'passive_port':port, 'error':e}
+        result_dict.update({'error1':e, 'result':True})
+        return result_dict
