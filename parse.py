dac11282b0c7212ec0042e1d141e5f2422aeb158
Siegfried148 <virgilio.castro.rendon@gmail.com>
2017-11-02 21:45:13 -0600

    crawler improved

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #Castro Rendón Virgilio
+from anytree import Node, RenderTree, Resolver
 
 def get_url(ip,port,protocol):
     if protocol == "HTTP":
@@ -14,6 +15,25 @@ def get_url(ip,port,protocol):
             url = "https://%s" % (ip)
     return url
 
+def add_to_structure(base, resource):
+#    print "*"*100
+#    print "/".join(resource)
+#    print base
+    try:
+        r = Resolver('r1')
+        try:
+            r.get(base, resource[0])
+        except Exception as e:
+            new_node = Node(resource[0],parent=base)
+            if len(resource) > 1:
+                return add_to_structure(new_node,resource[1:])
+            else:
+                return base
+    except Exception as e:
+        print e
+
+
+base_node = Node('www.seguridad.unam.mx', parent=None)
 with open('http_response.html','r') as i:
     url = get_url('www.seguridad.unam.mx','443','HTTPS')
     base = '\"'+url #The base is something like: "http://mysite.com
@@ -26,6 +46,9 @@ with open('http_response.html','r') as i:
                 #The last char of the url is '"'. The first index is added to get the index in the whole file
                 l_char_index = line[f_char_index:].index('"') + f_char_index  
                 url = line[f_char_index:l_char_index]
-                resource = url.split('//')[1]
-                print resource
+                resource = url.split('//')[1].split('/')[1:]
+                r_node = add_to_structure(base_node,resource)
+
+
+print RenderTree(r_node)
 
